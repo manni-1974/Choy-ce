f1992a994f01d999d0f301dd4be93907bfdf4048
Herman Fisher <hermanfisher@Hermans-MacBook-Pro-2.local>
2025-01-28 19:24:04 -0600

    Commit message describing your changes

--- blockchain_app.py
+++ blockchain_app.py
@@ -1,103 +1,150 @@
 from flask import Flask, jsonify, request
-import json
-from time import time
+import time
 import hashlib
-
-class Ifchain:
-    def __init__(self):
-        self.chain = []
-        self.transactions = []
-        self.create_block(proof=1, previous_hash='0')
-
-    def create_block(self, proof, previous_hash):
-        block = {
-            'index': len(self.chain) + 1,
-            'timestamp': time(),
-            'proof': proof,
-            'previous_hash': previous_hash,
-            'transactions': self.transactions
-        }
-        self.transactions = []
-        self.chain.append(block)
-        return block
-
-    def get_previous_block(self):
-        return self.chain[-1]
-
-    def proof_of_work(self, previous_proof):
-        new_proof = 1
-        check_proof = False
-        while not check_proof:
-            hash_operation = hashlib.sha256(
-                str(new_proof**2 - previous_proof**2).encode()).hexdigest()
-            if hash_operation[:4] == '0000':
-                check_proof = True
-            else:
-                new_proof += 1
-        return new_proof
-
-    def hash(self, block):
-        encoded_block = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(encoded_block).hexdigest()
-
-    def is_chain_valid(self, chain):
-        previous_block = chain[0]
-        block_index = 1
-        while block_index < len(chain):
-            block = chain[block_index]
-            if block['previous_hash'] != self.hash(previous_block):
-                return False
-            previous_proof = previous_block['proof']
-            proof = block['proof']
-            hash_operation = hashlib.sha256(
-                str(proof**2 - previous_proof**2).encode()).hexdigest()
-            if hash_operation[:4] != '0000':
-                return False
-            previous_block = block
-            block_index += 1
-        return True
-
+import json
+import schedule
+import threading
 
 app = Flask(__name__)
-ifchain = Ifchain()
 
+class PoH:
+    def __init__(self):
+        self.current_hash = self.generate_initial_hash()
+        self.history = []
 
-@app.route('/mine_block', methods=['GET'])
-def mine_block():
-    previous_block = ifchain.get_previous_block()
-    previous_proof = previous_block['proof']
-    proof = ifchain.proof_of_work(previous_proof)
-    previous_hash = ifchain.hash(previous_block)
-    block = ifchain.create_block(proof, previous_hash)
-    response = {
-        'message': 'Block mined successfully!',
-        'index': block['index'],
-        'timestamp': block['timestamp'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
-        'transactions': block['transactions']
-    }
-    return jsonify(response), 200
+    def generate_initial_hash(self):
+        return hashlib.sha256(str(time.time()).encode()).hexdigest()
 
+    def generate_hash(self):
+        new_hash = hashlib.sha256(self.current_hash.encode()).hexdigest()
+        self.history.append({"timestamp": time.time(), "hash": new_hash})
+        self.current_hash = new_hash
 
-@app.route('/get_chain', methods=['GET'])
+    def get_history(self):
+        return self.history
+
+class Block:
+    def __init__(self, index, timestamp, transactions, previous_hash, poh_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.transactions = transactions
+        self.previous_hash = previous_hash
+        self.poh_hash = poh_hash
+        self.nonce = 0
+
+    def compute_hash(self):
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+class IFChain:
+    difficulty = 2
+    transaction_tax_rate = 0.03
+
+    def __init__(self):
+        self.unconfirmed_transactions = []
+        self.chain = []
+        self.poh = PoH()
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        genesis_block = Block(0, time.time(), [], "0", self.poh.current_hash)
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+
+    def last_block(self):
+        return self.chain[-1]
+
+    def add_block(self, block, proof):
+        previous_hash = self.last_block().hash
+        if previous_hash != block.previous_hash:
+            return False
+        if not self.is_valid_proof(block, proof):
+            return False
+        block.hash = proof
+        self.chain.append(block)
+        return True
+
+    def is_valid_proof(self, block, block_hash):
+        return (block_hash.startswith('0' * IFChain.difficulty) and
+                block_hash == block.compute_hash())
+
+    def add_new_transaction(self, transaction):
+        tax = transaction['amount'] * IFChain.transaction_tax_rate
+        net_amount = transaction['amount'] - tax
+        if net_amount < 0:
+            return False
+        transaction['tax'] = tax
+        transaction['net_amount'] = net_amount
+        self.unconfirmed_transactions.append(transaction)
+        return True
+
+    def mine(self):
+        if not self.unconfirmed_transactions:
+            return False
+        last_block = self.last_block()
+        poh_hash = self.poh.current_hash
+        new_block = Block(index=last_block.index + 1,
+                          timestamp=time.time(),
+                          transactions=self.unconfirmed_transactions,
+                          previous_hash=last_block.hash,
+                          poh_hash=poh_hash)
+        proof = self.proof_of_work(new_block)
+        self.add_block(new_block, proof)
+        self.unconfirmed_transactions = []
+        return new_block.index
+
+    def proof_of_work(self, block):
+        block.nonce = 0
+        computed_hash = block.compute_hash()
+        while not computed_hash.startswith('0' * IFChain.difficulty):
+            block.nonce += 1
+            computed_hash = block.compute_hash()
+        return computed_hash
+
+ifchain = IFChain()
+
+def poh_generator():
+    while True:
+        ifchain.poh.generate_hash()
+        time.sleep(1)
+
+poh_thread = threading.Thread(target=poh_generator, daemon=True)
+poh_thread.start()
+
+@app.route('/chain', methods=['GET'])
 def get_chain():
-    response = {
-        'chain': ifchain.chain,
-        'length': len(ifchain.chain)
-    }
-    return jsonify(response), 200
+    chain_data = []
+    for block in ifchain.chain:
+        chain_data.append(block.__dict__)
+    return jsonify({"length": len(chain_data), "chain": chain_data})
 
-
-@app.route('/is_valid', methods=['GET'])
-def is_valid():
-    is_valid = ifchain.is_chain_valid(ifchain.chain)
-    if is_valid:
-        response = {'message': 'The blockchain is valid.'}
+@app.route('/add_transaction', methods=['POST'])
+def add_transaction():
+    tx_data = request.get_json()
+    required_fields = ["sender", "receiver", "amount", "token"]
+    if not all(field in tx_data for field in required_fields):
+        return "Invalid transaction data", 400
+    if ifchain.add_new_transaction(tx_data):
+        return "Transaction added to the pool", 201
     else:
-        response = {'message': 'The blockchain is not valid.'}
-    return jsonify(response), 200
+        return "Transaction invalid", 400
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    result = ifchain.mine()
+    if not result:
+        return "No transactions to mine", 400
+    return f"Block {result} is mined.", 200
+
+@app.route('/poh', methods=['GET'])
+def get_poh_history():
+    return jsonify(ifchain.poh.get_history())
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001)
+
+
+
+
